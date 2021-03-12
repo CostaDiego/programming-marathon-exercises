3ce8f5b2013a7f5968fa2ce38e1b1ca225a5abe2
Diego Costa <diego.szct@gmail.com>
2021-03-11 23:41:59 -0300

    Add query about items soluction

--- vjudge/train2/queries-about-elements/query-about-elements.cpp
+++ vjudge/train2/queries-about-elements/query-about-elements.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+int n,m;
+vector <int> a;
+vector <int> b;
+
 int main(){
-    return 0;
+    vector<int>::iterator itA;
+    vector<int>::iterator itB;
+
+    cin >> n >> m;
+
+    int tempA [n];
+    int tempB [m];
+
+    for(int i = 0; i < n; i++){
+        cin >> tempA[i];
+    }
+
+    for(int i = 0; i < m; i++){
+        cin >> tempB[i];
+    }
+
+    a.insert(a.begin(), tempA, tempA + n);
+    sort(a.begin(),a.end());
+
+    b.insert(b.begin(), tempB, tempB + m);
+    
+    for (itB = b.begin(); itB < b.end();itB++){
+        int out = upper_bound(a.begin(), a.end(),*itB) - a.begin();
+        cout << out << ' ';
+
+    }
+        
+
 }
