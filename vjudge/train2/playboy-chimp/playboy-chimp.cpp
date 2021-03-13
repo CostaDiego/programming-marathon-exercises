27b3d58981d4d96124f08f03e3c9affb93da12c6
Diego Costa <diego.szct@gmail.com>
2021-03-13 16:28:15 -0300

    Soluction to playboy chimp problem

--- vjudge/train2/playboy-chimp/playboy-chimp.cpp
+++ vjudge/train2/playboy-chimp/playboy-chimp.cpp
@@ -6,16 +6,40 @@ const ll N=201000;
 int sizeInputLadyChimps = 0;
 int sizeInputQueries = 0;
 
-vector <int> ladyChimps;
-vector <int> queries;
-
-vector <int>::iterator it;
-vector <int>::iterator smaller;
-vector <int>::iterator bigger;
-
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+
+    cin >> sizeInputLadyChimps;
+    vector <int> ladyChimps(sizeInputLadyChimps);
+
+    for (int i = 0; i < sizeInputLadyChimps; i++){
+        cin >> ladyChimps[i];
+    }
+
+    cin >> sizeInputQueries;
+    vector <int> queries(sizeInputQueries);
+
+    for (int i = 0; i < sizeInputQueries; i++){
+        cin >> queries[i];
+    }
+
+    for (int i = 0; i < sizeInputQueries; i++){
+        int smaller = lower_bound(ladyChimps.begin(), ladyChimps.end(),queries[i])-ladyChimps.begin();
+        int bigger = upper_bound(ladyChimps.begin(), ladyChimps.end(),queries[i]) - ladyChimps.begin();
+        
+        if(ladyChimps[smaller-1] == 0){
+            cout << "X ";
+        }else{
+            cout << ladyChimps[smaller-1] << " ";
+        }
+
+        if (ladyChimps[bigger] == 0){
+            cout << "X" << endl;
+        }else{
+            cout << ladyChimps[bigger] << endl;
+        }
+    }
 
     return 0;
 }
