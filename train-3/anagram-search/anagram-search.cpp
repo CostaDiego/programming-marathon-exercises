67394fb634be213afec0387ce507ef47a173d4f7
Diego Costa <diego.szct@gmail.com>
2021-03-26 19:13:26 -0300

    Clean soluction to base

--- train-3/anagram-search/anagram-search.cpp
+++ train-3/anagram-search/anagram-search.cpp
@@ -8,15 +8,5 @@ string secondLine;
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
-    // cin >> firstLine >> secondLine;
-
-
-    // for(size_t i = (secondLine.size() - 1); i < firstLine.size(); i++){
-    //     break;
-    // }
-    cout << 'a'%'?' << " " << 'z'%'?' << endl;
-    cout << (int)'?' << " " << (int)'a' <<  " " << (int)'z' << endl;
-    cout << (int)'aba' << " " << (int)'aab' << endl;
     return 0;
 }
