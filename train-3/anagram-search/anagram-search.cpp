ff28ed324add3433a91d3c6fb02f278b599de72e
Diego Costa <diego.szct@gmail.com>
2021-03-25 23:33:30 -0300

    Search anagram frist approach

---
+++ train-3/anagram-search/anagram-search.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define alphabet 26
+string firstLine;
+string secondLine;
+
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // cin >> firstLine >> secondLine;
+
+
+    // for(size_t i = (secondLine.size() - 1); i < firstLine.size(); i++){
+    //     break;
+    // }
+    cout << 'a'%'?' << " " << 'z'%'?' << endl;
+    cout << (int)'?' << " " << (int)'a' <<  " " << (int)'z' << endl;
+    cout << (int)'aba' << " " << (int)'aab' << endl;
+    return 0;
+}
