2051beccd9b9c824a311f1a2c377d5b1d301a12d
Diego Costa <diego.szct@gmail.com>
2021-04-25 20:36:33 -0300

    Feat(Train-5): Add folder to train 5 challenges

---
+++ train-5/raising-bacteria/raising-bacteria.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll N=201000;
+
+ll testCases = 0;
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
