afa59bf8b6aeede0b2c5f60918c36dbc89abe5d3
Diego Costa <diego.szct@gmail.com>
2021-04-25 21:49:56 -0300

    Feat(Raising-bacteria): Add solucton to raising bacteria problem

--- train-5/raising-bacteria/raising-bacteria.cpp
+++ train-5/raising-bacteria/raising-bacteria.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
-using namespace std;
 typedef long long ll;
 const ll N=201000;
 
-ll testCases = 0;
+using namespace std;
+
+ll bacteria = 0;
 
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+
+    cin >> bacteria;
+
+    cout << __builtin_popcount(bacteria) << endl;
 
     return 0;
 }
